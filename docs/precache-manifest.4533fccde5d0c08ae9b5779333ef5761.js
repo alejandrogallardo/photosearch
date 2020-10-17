a58f7d41b7f7393bb514a2160594c7ae31ccb688
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2020-10-17 13:19:21 -0600

    se agrego archivos docs

---
+++ docs/precache-manifest.4533fccde5d0c08ae9b5779333ef5761.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "067bc71bade6dcae942ede65d6bfdd74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ace1dd98eddfd98cc7d1",
+    "url": "/static/css/main.5171b26a.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "ace1dd98eddfd98cc7d1",
+    "url": "/static/js/main.96bbfcb0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
